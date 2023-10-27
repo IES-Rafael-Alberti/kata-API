2dde0ec50af030d52fbfbdba64e29259509d23a6
out0f4mm0 <eriking2004@gmail.com>
2023-10-27 10:02:00 +0200

    Init spring project

---
+++ src/main/java/com/example/cervezaapi/CervezaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.cervezaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CervezaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CervezaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cervezaapi/CervezaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cervezaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CervezaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
