30f7a400dfed83a3b4d2a4a7dc245a4c2dd9e7fa
Alexgf22 <agarcia@iesrafaelalberti.es>
2023-10-29 13:05:12 +0100

    Indicando packages direcciones en app principal y añadiendo varias dependencias necesarias

--- src/main/java/com/mi/appCervezas/AppDeCervezasApplication.java
+++ src/main/java/com/mi/appCervezas/AppDeCervezasApplication.java
@@ -2,8 +2,14 @@ package com.mi.appCervezas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.mi.appCervezas.models")
+@EnableJpaRepositories(basePackages = "com.mi.appCervezas.repositories")
+@ComponentScan(basePackages = {"com.mi.appCervezas.controllers", "com.mi.appCervezas.services"})
 public class AppDeCervezasApplication {
 
     public static void main(String[] args) {
