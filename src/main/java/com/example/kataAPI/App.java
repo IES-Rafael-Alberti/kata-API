3f4fc46322ec3c7ffb42a29c7aed134bfda4bbc4
Patricio <pcifrey501@g.educaand.es>
2023-11-01 20:59:47 +0100

    corregido fallo del initSQL, creada la springbootapplication y probada las dos funciones get de cerveza

---
+++ src/main/java/com/example/kataAPI/App.java
@@ -0,0 +1,22 @@
+package com.example.kataAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
+
+/*
+
+public class RestServiceApplication {
+
+
+
+}
+ */
--- src/main/java/com/example/kataAPI/controller/Controller_beers.java
+++ src/main/java/com/example/kataAPI/controller/Controller_beers.java
@@ -1,13 +1,37 @@
 package com.example.kataAPI.controller;
 
+import com.example.kataAPI.entity.Beer;
+import com.example.kataAPI.repo.repo_beer;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class Controller_beers {
 
-   // @GetMapping("/beers") // Muestra todas las cervezas	GET
-   // @GetMapping("/beer/{id}") // Mostrar la cerveza con el id {id}	GET
-   // @PostMapping("/beer") // Añadir una cerveza	POST
+    private final com.example.kataAPI.repo.repo_beer repo_beer;
+
+    public Controller_beers(repo_beer repo_beer) {
+        this.repo_beer = repo_beer;
+    }
+
+    @GetMapping("/beers") // Muestra todas las cervezas	GET
+    @ResponseBody
+    public List<Beer> get_all_beers(){
+        return repo_beer.findAll();
+    }
+    @GetMapping("/beer/{id}") // Mostrar la cerveza con el id {id}	GET
+    @ResponseBody
+    public Beer get_beer(@PathVariable Integer id){
+        return repo_beer.findById(id).orElse(null);
+    }
+    /*
+       @PostMapping("/beer") // Añadir una cerveza	POST
+    public Beer add_beer(){
+        //todo
+    }
+     */
+
    // @DeleteMapping("/beer/{id}") // Eliminar una cerveza DELETE
    // @PutMapping("/beer/{id}")  // Modificar parcialmente una cerveza PUT o PATCH
 
