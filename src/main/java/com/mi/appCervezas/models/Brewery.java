a5af2b2c6fc60781fc18399eb398efe056d3a013
Alexgf22 <agarcia@iesrafaelalberti.es>
2023-11-03 08:58:14 +0100

    Cambiando propiedades de las tablas de cada clase

--- src/main/java/com/mi/appCervezas/models/Brewery.java
+++ src/main/java/com/mi/appCervezas/models/Brewery.java
@@ -2,6 +2,7 @@ package com.mi.appCervezas.models;
 
 import jakarta.persistence.*;
 
+import java.util.Date;
 
 @Entity
 @Table(name = "breweries")
@@ -12,14 +13,46 @@ public class Brewery {
     @Column(nullable = false)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 255)
     private String name;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 255)
+    private String address1;
+
+    @Column(nullable = false, length = 255)
+    private String address2;
+
+    @Column(nullable = false, length = 255)
+    private String city;
+
+    @Column(nullable = false, length = 255)
+    private String state;
+
+    @Column(nullable = false, length = 25)
+    private String code;
+
+    @Column(nullable = false, length = 255)
     private String country;
 
+    @Column(nullable = false, length = 50)
+    private String phone;
+
+    @Column(nullable = false, length = 255)
+    private String website;
+
+    @Column(nullable = false, length = 255)
+    private String filepath;
+
     @Column(nullable = false)
-    private String address;
+    private String descript;
+
+    @Column(nullable = false, length = 11)
+    private int add_user;
+
+    @Column(nullable = false)
+    private Date last_mod;
+
+    // Getters y Setters
 
     public Long getId() {
         return id;
@@ -37,6 +70,46 @@ public class Brewery {
         this.name = name;
     }
 
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
     public String getCountry() {
         return country;
     }
@@ -45,15 +118,51 @@ public class Brewery {
         this.country = country;
     }
 
-    public String getAddress() {
-        return address;
+    public String getPhone() {
+        return phone;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 
+    public String getWebsite() {
+        return website;
+    }
 
+    public void setWebsite(String website) {
+        this.website = website;
+    }
 
+    public String getFilepath() {
+        return filepath;
+    }
+
+    public void setFilepath(String filepath) {
+        this.filepath = filepath;
+    }
+
+    public String getDescript() {
+        return descript;
+    }
+
+    public void setDescript(String descript) {
+        this.descript = descript;
+    }
+
+    public int getAdd_user() {
+        return add_user;
+    }
+
+    public void setAdd_user(int add_user) {
+        this.add_user = add_user;
+    }
+
+    public Date getLast_mod() {
+        return last_mod;
+    }
+
+    public void setLast_mod(Date last_mod) {
+        this.last_mod = last_mod;
+    }
 }
-
--- src/main/java/com/mi/appCervezas/models/Category.java
+++ src/main/java/com/mi/appCervezas/models/Category.java
@@ -2,6 +2,8 @@ package com.mi.appCervezas.models;
 
 import jakarta.persistence.*;
 
+import java.util.Date;
+
 @Entity
 @Table(name = "categories")
 public class Category {
@@ -10,11 +12,11 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private String name;
+    @Column(nullable = false, length = 255)
+    private String cat_name;
 
     @Column(nullable = false)
-    private String last_mod;
+    private Date last_mod;
 
     public Long getId() {
         return id;
@@ -24,20 +26,19 @@ public class Category {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getCat_name() {
+        return cat_name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setCat_name(String cat_name) {
+        this.cat_name = cat_name;
     }
 
-    public String getLast_mod() {
+    public Date getLast_mod() {
         return last_mod;
     }
 
-    public void setLast_mod(String last_mod) {
+    public void setLast_mod(Date last_mod) {
         this.last_mod = last_mod;
     }
 }
-
