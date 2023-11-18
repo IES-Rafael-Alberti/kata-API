c446bfda948f79abd8bdcebcfda6b03d117004e7
Alexgf22 <agarcia@iesrafaelalberti.es>
2023-11-18 12:39:39 +0100

    Quitando imports innecesarios

--- src/main/java/com/mi/appCervezas/controllers/CategoryController.java
+++ src/main/java/com/mi/appCervezas/controllers/CategoryController.java
@@ -1,13 +1,14 @@
 package com.mi.appCervezas.controllers;
 
 import com.mi.appCervezas.dto.CategoryDTO;
-import com.mi.appCervezas.error.CategoryNotFoundException;
-import com.mi.appCervezas.models.Category;
 import com.mi.appCervezas.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
