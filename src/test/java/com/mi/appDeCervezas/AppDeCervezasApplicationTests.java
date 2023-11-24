0310dceafcc242a3ca37be54dfd4e193c04524e7
Alexgf22 <agarcia@iesrafaelalberti.es>
2023-11-24 10:50:07 +0100

    cambios

---
+++ src/test/java/com/mi/appDeCervezas/AppDeCervezasApplicationTests.java
@@ -0,0 +1,13 @@
+/*package com.mi.appDeCervezas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDeCervezasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}*/
